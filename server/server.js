f934f9611307c85b0c361a35f2d7b512175474aa
fiverwork10 <fiverwork10@gmail.com>
2026-07-15 01:03:42 -0700

    Revert 0.0.0.0 to default

--- server/server.js
+++ server/server.js
@@ -347,7 +347,7 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, '0.0.0.0', () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   initWhatsApp();
 });
